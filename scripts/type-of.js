638698e8b74f4424c709376290ee7edb98fea62e
Bingo Bobo <matmazur90@gmail.com>
2018-10-15 12:50:09 +0200

    moved previous script to individual file, checking how variables can be changed to different type without any problemg

---
+++ scripts/type-of.js
@@ -0,0 +1,8 @@
+console.log(typeof 15);
+console.log(typeof 432.3);
+console.log(typeof "Mike");
+console.log(typeof 'mike');
+console.log(typeof null);
+console.log(typeof [14.3, 543, 3]);
+console.log(typeof b);
+console.error("Error!");
