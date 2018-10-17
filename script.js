0f8b8ddf45404bd629c3ea6ff537673e928b7d68
Bingo Bobo <matmazur90@gmail.com>
2018-10-17 12:27:41 +0200

    added functions making button responsive for click sending input msg to the list

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-function showNewsHeading(){
-    
-    var heading  = document.getElementById("news");
+/*eslint-env browser*/
+/*eslint no-console: ["error", { allow: ["warn", "error","log"] }] */
+
+function showNewsHeading() {
+
+    var heading = document.getElementById("news");
     console.log(heading.innerHTML);
 }
 
-function showNewsListItems(){
-    
-     var heading  = document.getElementById("news-list");
-    
+function showNewsListItems() {
+
+    var heading = document.getElementById("news-list");
+
     console.log(heading.getElementsByTagName("li")[1].innerHTML);
 }
 
-function changeHeadings(){
+function changeHeadings() {
     var news = document.getElementById("news");
     var sport = document.getElementById("sport");
-    
+
     news.innerHTML = "Happy Time news";
     sport.innerHTML = "Bad sport you are Ricky"
+}
+
+
+
+function addNews() {
+    var ul = document.getElementById("news-list");
+
+
+    var input = document.getElementById("input-text");
+    var news = input.value;
+
+    if (news && news !== "") {
+
+        var liText = document.createTextNode(news);
+
+        var li = document.createElement("li");
+
+        li.appendChild(liText);
+        ul.appendChild(li);
+
+        input.value = "";
+        console.log(news);
+    }
+}
+
+function registerEvents() {
+    var button = document.getElementById("add-button");
+
+    console.log(button.innerHTML);
+
+    button.onclick = function () {
+        addNews();
+    }
+
 }
