a63258e15680448a821ca25e6a19718f39bd50e2
Bingo Bobo <matmazur90@gmail.com>
2018-10-21 18:36:20 +0200

    placed Book constructor in factory object

--- js/app/app.js
+++ js/app/app.js
@@ -1,23 +1,27 @@
 var app = angular.module('app', []);
 
-app.controller("BookController", function () {
 
+app.factory("Book", function () {
     function Book(title, author, isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
-
     }
+    return Book;
+});
+
+
+app.controller("BookController", function (Book) {
+
 
     this.books = [
-
-    new Book("Moby Dick", "Hemingway", "5425DFSFGsd"),
+     new Book("Moby Dick", "Hemingway", "5425DFSFGsd"),
      new Book("Release Dick", "Ernest", "sdfsDFD"),
      new Book("Moby", "Moby", "^$%$3433")
 ];
 
-    this.addBook = function(book) {
-        this.books.push(new Book(book.title,book.author,book.isbn));
+    this.addBook = function (book) {
+        this.books.push(new Book(book.title, book.author, book.isbn));
     };
 
 
