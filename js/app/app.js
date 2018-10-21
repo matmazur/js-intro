8803e2d6fe26a2548dd260b15ae63c59364161ef
Bingo Bobo <matmazur90@gmail.com>
2018-10-21 20:07:51 +0200

    added few more check if null statements

--- js/app/app.js
+++ js/app/app.js
@@ -21,7 +21,14 @@ app.controller("BookController", function (Book) {
 ];
 
     this.addBook = function (book) {
+        if (this.book.title!=null && this.book.author!=null){
         this.books.push(new Book(book.title, book.author, book.isbn));
+
+        this.book.title = "";
+        this.book.author = "";
+        this.book.isbn = "";
+    }
+
     };
 
 
