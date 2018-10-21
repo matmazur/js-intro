a4a71565ee114b02783ff09a204fe75167abe051
Bingo Bobo <matmazur90@gmail.com>
2018-10-21 18:17:38 +0200

    created object constructor Book in bookcontrolelr with list of books and iterated on it on html

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,21 @@
 var app = angular.module('app', []);
 
+app.controller("BookController", function() {
+
+    function Book(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
+    }
+
+    this.books = [
+
+    new Book("Moby Dick", "Hemingway", "5425DFSFGsd"),
+     new Book("Release Dick", "Ernest", "sdfsDFD"),
+     new Book("Moby", "Moby", "^$%$3433")
+];
 
 
+
+});
